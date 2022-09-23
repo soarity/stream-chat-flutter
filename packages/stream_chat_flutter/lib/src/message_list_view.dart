15ba648d4c226cb8a795c03004927ea132c03e9a
Aanu Olakunle <olakunleaanu@gmail.com>
2022-09-23 14:51:53 +0100

    fix

--- packages/stream_chat_flutter/lib/src/message_list_view.dart
+++ packages/stream_chat_flutter/lib/src/message_list_view.dart
@@ -731,7 +731,8 @@ class _StreamMessageListViewState extends State<StreamMessageListView> {
 
     final backgroundColor =
         StreamMessageListViewTheme.of(context).backgroundColor;
-    final backgroundImage = StreamMessageListViewTheme.of(context).backgroundImage;
+    final backgroundImage =
+        StreamMessageListViewTheme.of(context).backgroundImage;
 
     if (backgroundColor != null || backgroundImage != null) {
       return DecoratedBox(
